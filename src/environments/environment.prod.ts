ac59873b379edd0b020f183292e39701088b2cd9
denis zaytcev <d.zaycev@software2life.com>
2019-03-04 10:43:31 +0300

    Адрес для миниатюр

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
     API_BASE_PATH: 'https://openlibrary.org',
-    COVER_BASE_PATH: 'http://covers.openlibrary.org',
+    COVER_BASE_PATH: 'https://covers.openlibrary.org',
 };
